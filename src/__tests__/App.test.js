a5b8d0494017e974d5dae7ef8d7840a885b8ed85
ian ayiro <ian.ayiro@student.moringa.com>
2024-10-08 14:31:20 +0300

    modified:   src/__tests__/App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,28 +3,28 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../components/App";
 
 test("displays in 'light' mode when initialized", () => {
-  const { container } = render(<App />);
-  expect(container.querySelector(".App.light")).toBeInTheDocument();
+  const { screen } = render(<App />);
+  expect(screen.getByRole(".App.light")).toBeInTheDocument();
 });
 
 test("changes to 'dark' mode when the button is clicked", () => {
-  const { container } = render(<App />);
-  expect(container.querySelector(".App.light")).toBeInTheDocument();
+  const { screen } = render(<App />);
+  expect(screen.getByRole(".App.light")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".App.dark")).toBeInTheDocument();
+  expect(screen.getByRole(".App.dark")).toBeInTheDocument();
 });
 
 test("changes back to 'light' mode when the button is clicked twice", () => {
-  const { container } = render(<App />);
-  expect(container.querySelector(".App.light")).toBeInTheDocument();
+  const { screen } = render(<App />);
+  expect(screen.getByRole(".App.light")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".App.dark")).toBeInTheDocument();
+  expect(screen.getByRole(".App.dark")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".App.light")).toBeInTheDocument();
+  expect(screen.getByRole(".App.light")).toBeInTheDocument();
 });
